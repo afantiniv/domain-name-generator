77abad00ca02632ece739cb73f722c830222f85d
afantiniv <fantiniv95@gmail.com>
2022-11-04 05:36:50 +0000

    Vista en lista

--- src/app.js
+++ src/app.js
@@ -11,18 +11,27 @@ var noun = ["jogger", "racoon"];
 let puntos = [".com", ".net", ".cr", ".es"];
 
 function generar() {
-  let dominio = [];
+  let dominio = "";
+  let dominios = "";
   for (let x = 0; x < pronoun.length; x++) {
     for (let y = 0; y < adj.length; y++) {
       for (let z = 0; z < noun.length; z++) {
         for (let w = 0; w < puntos.length; w++) {
           console.log(pronoun[x] + adj[y] + noun[z]);
-          dominio.push(pronoun[x] + adj[y] + noun[z] + puntos[w] + "<br>");
+          //dominio.push(pronoun[x] + adj[y] + noun[z] + puntos[w] + "<br>");
+          dominio = pronoun[x] + adj[y] + noun[z] + puntos[w];
+          dominios =
+            dominios +
+            "<li class=" +
+            "list-group-item" +
+            ">" +
+            dominio +
+            "</li>";
         }
       }
     }
   }
-  document.getElementById("texto").innerHTML = dominio.join("br");
+  document.getElementById("lista").innerHTML = dominios;
 }
 
 document.getElementById("boton").onclick = function() {
@@ -30,7 +39,7 @@ document.getElementById("boton").onclick = function() {
 };
 
 document.getElementById("limpiar").onclick = function() {
-  let ele = document.getElementById("texto");
+  let ele = document.getElementById("lista");
   ele.childElementCount;
   while (ele.lastChild) {
     ele.lastChild.remove();
