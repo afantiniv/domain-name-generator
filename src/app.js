9688c8a3acf2392a9e89d20358c79d084a39ff89
afantiniv <fantiniv95@gmail.com>
2022-11-04 02:55:48 +0000

    Generador de dominios listo

--- src/app.js
+++ src/app.js
@@ -5,7 +5,35 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+var pronoun = ["the", "our"];
+var adj = ["great", "big"];
+var noun = ["jogger", "racoon"];
+let puntos = [".com", ".net", ".cr", ".es"];
+
+function generar() {
+  let dominio = [];
+  for (let x = 0; x < pronoun.length; x++) {
+    for (let y = 0; y < adj.length; y++) {
+      for (let z = 0; z < noun.length; z++) {
+        for (let w = 0; w < puntos.length; w++) {
+          console.log(pronoun[x] + adj[y] + noun[z]);
+          dominio.push(pronoun[x] + adj[y] + noun[z] + puntos[w] + "<br>");
+        }
+      }
+    }
+  }
+  document.getElementById("texto").innerHTML = dominio;
+}
+
+document.getElementById("boton").onclick = function() {
+  generar();
+};
+
+document.getElementById("limpiar").onclick = function() {
+  let ele = document.getElementById("texto");
+  ele.childElementCount;
+  while (ele.lastChild) {
+    ele.lastChild.remove();
+  }
+  ele.childElementCount;
 };
